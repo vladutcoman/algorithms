271fabf2f528355aa00a746e7290da46bf2f76f0
acoman <andrei-vladut.coman@ionos.com>
2019-01-08 16:31:30 +0200

    Longest common susequence

---
+++ src/youtube/LongestCommonSubsequence.java
@@ -0,0 +1,29 @@
+package youtube;
+
+public class LongestCommonSubsequence {
+
+    public static int lcs(char[] x, char[] y, int m, int n) {
+        int L[][] = new int[m+1][n+1];
+
+    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
+         that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+        for (int i=0; i<=m; i++)
+        {
+            for (int j=0; j<=n; j++)
+            {
+                if (i == 0 || j == 0)
+                    L[i][j] = 0;
+                else if (x[i-1] == y[j-1])
+                    L[i][j] = L[i-1][j-1] + 1;
+                else
+                    L[i][j] = max(L[i-1][j], L[i][j-1]);
+            }
+        }
+        return L[m][n];
+    }
+
+    static int max(int a, int b)
+    {
+        return (a > b)? a : b;
+    }
+}
