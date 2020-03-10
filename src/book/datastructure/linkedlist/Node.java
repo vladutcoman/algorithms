6c8975df37f3bc4bc36ac5d90669268c59cc491e
Coman Vladut <comanvladut@Comans-MacBook-Pro.local>
2020-03-10 21:23:22 +0200

    linked list create

---
+++ src/book/datastructure/linkedlist/Node.java
@@ -0,0 +1,34 @@
+package book.datastructure.linkedlist;
+
+public class Node {
+    private Node next= null;
+    private int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public void appendToTail(int d) {
+        Node end = new Node(d);
+        Node n = this;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    public Node deleteNode(Node head, int d) {
+        Node n = head;
+        if (n.data == d) {
+            return head.next;
+        }
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+        return head;
+    }
+}
